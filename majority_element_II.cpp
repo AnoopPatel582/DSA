eb21864bbceee2d068aa14d23545665d01731921
Anoop Patel <anooppatel582@gmail.com>
2026-01-05 14:39:18 +0530

    Solution of the finding all majority elements which are greater than n/3. Leetcode problem number 229.

---
+++ majority_element_II.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> majorityElement(vector<int>& nums){
+    int count1=0,count2=0,candidate1, candidate2;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        if(candidate1==nums[i])
+            count1++;
+        else if(candidate2==nums[i])
+            count2++;
+        else if(count1==0){
+            candidate1=nums[i];
+            count1++;
+        }
+        else if(count2==0){
+            candidate2=nums[i];
+            count2++;
+        }
+        else{
+            count1--;
+            count2--;
+        }
+    }
+    count1=0,count2=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]==candidate1)
+            count1++;
+        else if(nums[i]==candidate2)
+            count2++;
+    }
+    vector<int>result(0);
+    if(count1>n/3)
+        result.push_back(candidate1);
+    if(count2>n/3)
+        result.push_back(candidate2);
+    return result;
+}
+
+int main(){
+    vector<int> nums = {1};
+    vector<int> majority = majorityElement(nums);
+    cout<<"Majority Elements are ";
+    for(int num:majority){
+        cout<<num<<" ";
+    }
+}
